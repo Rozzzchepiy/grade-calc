79a19d90f05362e7b0dd095b2837aa620cf6e60a
Rozzzchepiy <rozchepijyura@gmail.com>
2026-05-12 13:55:52 +0300

    fix: 404

--- src/main.jsx
+++ src/main.jsx
@@ -34,12 +34,9 @@ Sentry.setUser({
 
 // Ініціалізація PostHog (з реверс-проксі для Vercel)
 posthog.init(POSTHOG_KEY, {
-    api_host: isProduction ? '/ingest' : 'https://eu.i.posthog.com',
-    ui_host: 'https://eu.posthog.com',
+    api_host: 'https://eu.i.posthog.com',
     person_profiles: 'always' 
 });
-
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
